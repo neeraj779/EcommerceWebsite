4215deea165457bcf6c908fff7e25ea4980e888b
Neeraj <neeraj9108@gmail.com>
2021-07-17 14:50:17 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
