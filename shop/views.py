c3613783c2ed9a1a652072a854c47c4139aa4a5b
Neeraj <neeraj9108@gmail.com>
2021-07-23 14:26:00 +0530

    added layout of shop page

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,24 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return render(request,'shop/index.html')
+
+def contact(request):
+    return render(request,'shop/index.html')
+
+def tracker(request):
+    return render(request,'shop/index.html')
+
+def search(request):
+    return render(request,'shop/index.html')
+
+def productView(request):
+    return render(request,'shop/index.html')
+
+def checkout(request):
+    return render(request,'shop/index.html')
+
+def handlerequest(request):
+    return render(request,'shop/index.html')
