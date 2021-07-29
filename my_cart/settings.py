22511e67384f366af6ccb26a92dac6d72ff93846
Neeraj <neeraj9108@gmail.com>
2021-07-29 10:45:20 +0530

    Update settings.py

--- my_cart/settings.py
+++ my_cart/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 with open('key.txt', 'r') as key:
     django_key = key.read() 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = django_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
